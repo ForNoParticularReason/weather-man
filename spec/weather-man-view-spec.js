b55d4fcfab34fef8ca6a20849f4e4dea326950fb
Gzopel <jepunto@gmail.com>
2018-10-03 15:15:29 -0300

    init

---
+++ spec/weather-man-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import WeatherManView from '../lib/weather-man-view';
+
+describe('WeatherManView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
